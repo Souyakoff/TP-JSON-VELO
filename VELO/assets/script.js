a6c9179dd4a821101c55fa8a1f9613220acc5ca2
CAFFENNE Hugo <158146784+Souyakoff@users.noreply.github.com>
2024-09-26 11:20:21 +0200

    Update script.js

--- VELO/assets/script.js
+++ VELO/assets/script.js
@@ -1,6 +1,7 @@
 /* Debug des données */
 console.table(data);
 
+// Initialisation de la carte avec vue centrée sur La Rochelle
 let map = L.map('map').setView([46.1635705075646, -1.1268395422898], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,18 +9,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Parcourir les données pour chaque station
 data.forEach(currentItem => {
+    // Affichage de la station et de l'adresse 
     console.log("Station :");
     console.info(currentItem.fields.station_nom + " " + currentItem.fields.station_adresse);
+
+    // Affichage des coordonnées de la station 
     console.log("Coordonnées :");
     console.log("Latitude : " + currentItem.fields.station_latitude);
     console.log("Longitude : " + currentItem.fields.station_longitude);
 
+    // Création d'un marqueur sur la carte avec les coordonnées latitude/longitude
     let marker = L.marker([
-        parseFloat(currentItem.fields.station_latitude.replace(',', '.')),
-        parseFloat(currentItem.fields.station_longitude.replace(',', '.'))
+        parseFloat(currentItem.fields.station_latitude.replace(',', '.')),  // Latitude
+        parseFloat(currentItem.fields.station_longitude.replace(',', '.'))  // Longitude
     ]).addTo(map);
 
+    // Ajout d'une popup pour chaque marqueur avec les informations de la station
     marker.bindPopup(
         `<b>${currentItem.fields.station_nom}</b><br>${currentItem.fields.station_adresse}<br>
         <b>Latitude :</b> ${currentItem.fields.station_latitude}<br>
@@ -27,3 +34,4 @@ data.forEach(currentItem => {
         <b>Vélos disponibles :</b> ${currentItem.fields.velos_disponibles}`
     );
 });
+
